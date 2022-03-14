01b9961cd27ed7f302d7bda18c73085f82d06bb2
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-03-14 17:08:39 +0000

    Initial Commit

---
+++ MatheusCard/MatheusCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatheusCardApp.swift
+//  MatheusCard
+//
+//  Created by Matheus Diniz on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MatheusCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatheusCardUITests/MatheusCardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MatheusCardUITestsLaunchTests.swift
+//  MatheusCardUITests
+//
+//  Created by Matheus Diniz on 14/03/2022.
+//
+
+import XCTest
+
+class MatheusCardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
